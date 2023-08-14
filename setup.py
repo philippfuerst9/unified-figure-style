b08c72a1b42e1c8a21e489ebf34db29aae62b5a0
Philipp Fuerst <philipp.fuerst@rwth-aachen.de>
2023-08-14 08:34:49 -0500

    restructure to installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ufig',
+    version='0.1.0',
+    description='Unified figure style tools',
+    url='https://github.com/philippfuerst9/unified-figure-style',
+    author='Philipp Fuerst',
+    install_requires=['matplotlib', 'numpy']
+)
---
+++ ufig/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.0"
+__author__ = 'Philipp Fuerst'
+
+from ufig.ufig import FigureHandler, cycler, full_step, get_ratio_error, lighten_color, linestyles, plot_hist, plot_ratio_double_err, plot_ratio_single_err, rolling_window, rolling_window_2d, write_preliminary
