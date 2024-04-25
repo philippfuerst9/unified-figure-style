115441fe0dbd2736669852472ab1b6fb19b65c0f
Philipp Fuerst <philipp.fuerst@rwth-aachen.de>
2024-04-25 08:43:37 -0500

    imports and ignore files cleanup

--- ufig/__init__.py
+++ ufig/__init__.py
@@ -2,4 +2,5 @@ __version__ = "0.2.0"
 __author__ = 'Philipp Fuerst'
 
 from ufig.ufig import FigureHandler, draw_numbers, cycler, full_step, get_ratio_error, lighten_color, linestyles, plot_hist, plot_ratio_double_err, plot_ratio_single_err, rolling_window, rolling_window_2d, write_preliminary
-from ufig.globalfit_plotformatter import PlotFormatter
+from ufig.globalfit_recospace_plotformatter import PlotFormatter
+from ufig.scanplotter import ScanPlotter
