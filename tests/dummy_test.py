315efa1c8662ba7c9e34c6378f83246ac3c412b9
Philipp Fuerst <philipp.fuerst@rwth-aachen.de>
2024-12-09 14:18:53 -0600

    move tests

---
+++ tests/dummy_test.py
@@ -0,0 +1,8 @@
+from ufig.figure_helpers import square
+
+def test_square():
+    """write asser statements in tests --> becomes
+    error message later"""
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(4) == 16 
